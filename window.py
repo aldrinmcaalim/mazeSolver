800f6ea7014649aa87258dd1f9712fd79c9ac047
aldrinmcaalim <aldrinmcaalim@gmail.com>
2022-10-28 13:35:31 -0400

    Adding project files, initialized window constructor class

---
+++ window.py
@@ -0,0 +1,11 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+    def __init__(self, width, height):
+        self.__root = Tk()
+        self.__root.title("Maze Solver")
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.__running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        
