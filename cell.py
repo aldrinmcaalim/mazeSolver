2a6ba37db3670ac7be25683f3e9534933538ad23
aldrinmcaalim <aldrinmcaalim@gmail.com>
2022-10-28 21:53:26 -0400

    created Cell class

---
+++ cell.py
@@ -0,0 +1,16 @@
+from graphics import Line, Point
+
+class Cell:
+    def __init__(self, win):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self.visited = False
+        self._x1 = None
+        self.__x2 = None
+        self.__y1 = None
+        self.__y2 = None
+        self.__win = win
+
+    
