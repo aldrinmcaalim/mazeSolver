7c90ac9971a390c47d3957851b8be0faf49a81b5
aldrinmcaalim <aldrinmcaalim@gmail.com>
2022-10-29 05:53:59 -0400

    fixed col_cell to be col_cells

--- maze.py
+++ maze.py
@@ -3,7 +3,16 @@ import random
 import time 
 
 class Maze:
-    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win=None):
+    def __init__(
+        self, 
+        x1, 
+        y1, 
+        num_rows, 
+        num_cols, 
+        cell_size_x, 
+        cell_size_y, 
+        win=None
+    ):
         self._cells = []
         self._x1 = x1
         self._y1 = y1
@@ -12,10 +21,12 @@ class Maze:
         self._cell_size_x = cell_size_x
         self._cell_size_y = cell_size_y
         self._win = win
+    
+        self._create_cells()
 
     def _create_cells(self):
         for i in range(self._num_cols):
-            col_cell = []
+            col_cells = []
             for j in range(self._num_rows):
                 col_cells.append(Cell(self._win))
             self._cells.append(col_cells)
@@ -26,9 +37,8 @@ class Maze:
     def _draw_cell(self, i, j):
         if self._win is None:
             return
-        
         x1 = self._x1 + i * self._cell_size_x
-        y1 = self._y1 + j * self._cell_size_y
+        y1 = self._y1 + i * self._cell_size_y
         x2 = x1 + self._cell_size_x
         y2 = y1 + self._cell_size_y
         self._cells[i][j].draw(x1, y1, x2, y2)
